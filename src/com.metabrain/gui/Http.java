0bff8277eba502770d7dfc4c92a92907872ea441
mgaiduk <ext-mgaiduk@parkeon.com>
2018-11-21 13:59:29 +0300

    delete mac getting

--- src/com.metabrain/gui/Http.java
+++ src/com.metabrain/gui/Http.java
@@ -11,40 +11,9 @@ public class Http extends NanoHTTPD {
         super(port);
     }
 
-    String getMac(InetAddress ip){
-        try {
-            ip = InetAddress.getLocalHost();
-            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-            byte[] mac = network.getHardwareAddress();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < mac.length; i++)
-                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
-            return sb.toString();
-
-        } catch (Exception e) {
-            return "error";
-        }
-    }
-
     @Override
     public Response serve(IHTTPSession session) {
-        String ss;
-        try {
-            ss = getMac(InetAddress.getByName(session.getHeaders().get("remote-addr")));
-        } catch (UnknownHostException e) {
-            ss = "get";
-        }
-        System.out.println(session.getHeaders().get("remote-addr"));
-        return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, ss);
+        return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "hello world");
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        NanoHTTPD ds = new Http(20000);
-        try {
-            ds.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Thread.sleep(100000);
-    }
 }
