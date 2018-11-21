49db15451800121a3a9b019fcd75e38a1d1f52d1
mgaiduk <ext-mgaiduk@parkeon.com>
2018-11-21 13:13:53 +0300

    configure project

---
+++ test/com/metabrain/gui/HttpTest.java
@@ -0,0 +1,14 @@
+package com.metabrain.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpTest {
+
+    @Test
+    void getMac() {
+        assertEquals(1, 1);
+    }
+
+}
