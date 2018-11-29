c39ea49d9a3b02b01465e98b1a3504d66b41e463
mgaiduk <ext-mgaiduk@parkeon.com>
2018-11-29 15:42:39 +0300

    change test res dir

--- test/com/metabrain/gui/server/DjsThreadTest.java
+++ test/com/metabrain/gui/server/DjsThreadTest.java
@@ -17,7 +17,7 @@ class DjsThreadTest {
 
     @Test
     void setNode() throws IOException {
-        File file = new File("test/res/update/node.json");
+        File file = new File("test_res/node.json");
         String nodeBody = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
         GetNodeBody getNodeBody = json.fromJson(nodeBody, GetNodeBody.class);
         DjsThread.updateNode(getNodeBody.body);
