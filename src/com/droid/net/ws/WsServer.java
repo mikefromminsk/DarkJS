5600e4c16422be19295e69e32233f5bcc50fff06
mgaiduk <ext-mgaiduk@parkeon.com>
2019-06-21 14:12:00 +0300

    refactor

--- src/com/droid/net/ws/WsServer.java
+++ src/com/droid/net/ws/WsServer.java
@@ -4,9 +4,7 @@ import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
@@ -56,16 +54,6 @@ public class WsServer extends WebSocketServer {
         WsServer s = new WsServer(port);
         s.start();
         System.out.println("WsServer started on port: " + s.getPort());
-
-        BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String in = sysin.readLine();
-            s.broadcast(in);
-            if (in.equals("exit")) {
-                s.stop(1000);
-                break;
-            }
-        }
     }
 
     @Override
